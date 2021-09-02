73a54edf7d382d90c8fa877fd7798c623320d5dc
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-09-02 10:58:06 -0600

    Deployable

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+console.log(process.env.USER);
+console.log(process.env.HOME);
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 5000;
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send(`<h1>Web 45 ROCKS!</h1>`);
+});
+
+server.get("/api", (req, res) => {
+  res.json({ message: "Web 45 is awesome!" });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
